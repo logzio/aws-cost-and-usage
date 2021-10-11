2527b566a0bb7131a08d0d14d208d964d357f458
ShiranAvidov <48657224+ShiranAvidov@users.noreply.github.com>
2021-10-11 12:56:46 +0300

    Update run.py

--- action/run.py
+++ action/run.py
@@ -15,20 +15,10 @@ AUTO_DEPLOYMENT_YAML_FILE_NAME = 'auto-deployment.yaml'
 AUTO_DEPLOYMENT_YAML_FILE_LOCAL_PATH = "{0}/{1}".format(os.environ['SOURCE_DIR'], AUTO_DEPLOYMENT_YAML_FILE_NAME)
 AUTO_DEPLOYMENT_YAML_FILE_S3_PATH = "{0}/{1}".format(os.environ['FOLDER_NAME'], AUTO_DEPLOYMENT_YAML_FILE_NAME)
 
-s3client = boto3.client('s3')
-
-
-def delete_bucket_folder_files(bucket, folder):
-    s3resource = boto3.resource('s3')
-    bucket = s3resource.Bucket(bucket)
-
-    bucket.objects.filter(Prefix=folder).delete()
-    s3client.put_object(Bucket=bucket, Key=(folder + '/'))
-
-    print("S3 bucket directory is now empty")
-
 
 def upload_to_aws(local_file, bucket, s3_file):
+    s3client = boto3.client('s3')
+    
     try:
         s3client.upload_file(local_file, bucket, s3_file, ExtraArgs={'ACL': 'public-read'})
         print("{} was uploaded successfully".format(s3_file))
@@ -41,7 +31,6 @@ def upload_to_aws(local_file, bucket, s3_file):
 
 
 def main():
-    delete_bucket_folder_files(os.environ['AWS_S3_BUCKET'], os.environ['FOLDER_NAME'])
     upload_to_aws(LAMBDA_FUNCTION_CODE_ZIP_FILE_LOCAL_PATH, os.environ['AWS_S3_BUCKET'], LAMBDA_FUNCTION_CODE_ZIP_FILE_S3_PATH)
     upload_to_aws(AUTO_DEPLOYMENT_YAML_FILE_LOCAL_PATH, os.environ['AWS_S3_BUCKET'], AUTO_DEPLOYMENT_YAML_FILE_S3_PATH)
 
